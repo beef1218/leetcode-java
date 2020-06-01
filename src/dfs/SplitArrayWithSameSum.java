806c81fd11fbb8af4aee56d4e98c8d6073745c3d
andy.liu <andy.liu@servicenow.com>
2020-06-01 16:10:16 -0700

    Split arrays

--- src/dfs/SplitArrayWithSameSum.java
+++ src/dfs/SplitArrayWithSameSum.java
@@ -8,71 +8,61 @@ Given an input array with integers, how to split the array into k subarrays such
  */
 /*
 dfs strategy
-
+1. get total sum and divide k to get the sum of each subarray
+2. dfs
+3. return a list of list of endIndex of the split position.
 recursion tree:
 number of levels: k
 each node has up to n - k + level number of branches
 
-base case: level == k
-at each level: use a loop to keep adding elements, when current sum == target sum, do recursion
-Note: at first chunk, we do not have a sum, so we use MIN_VALUE as a special value to indicate the first chunk
- */
+base case: level == k || end of array is reached
+at each level: use a for loop to get curSum, when curSum == target sum, do recursion
+*/
 public class SplitArrayWithSameSum {
 	public static void main(String[] args) {
 		SplitArrayWithSameSum solution = new SplitArrayWithSameSum();
 		// test case 1
 		int[] input = new int[]{3, -1, 4, 6, -8, 1, 1};
 		int k = 3;
-		List<List<List<Integer>>> result = solution.splitArray(input, k);
+		List<List<Integer>> result = solution.splitArray(input, k);
 		System.out.println(result);
 
 		// test case 2
 		int[] input2 = new int[]{0, 0, 0, 0, 0};
 		int k2 = 3;
-		List<List<List<Integer>>> result2 = solution.splitArray(input2, k2);
+		List<List<Integer>> result2 = solution.splitArray(input2, k2);
 		System.out.println(result2);
 	}
-	public List<List<List<Integer>>> splitArray(int[] input, int k) {
-		List<List<List<Integer>>> result = new ArrayList<>();
+
+	public List<List<Integer>> splitArray(int[] input, int k) {
+		List<List<Integer>> result = new ArrayList<>();
 		if (input == null || input.length < k || k <= 1) {
 			return result;
 		}
-		dfs(0, 0, Integer.MIN_VALUE, input, k, new ArrayList<List<Integer>>(), result);
+		int totalSum = 0;
+		for (int num : input) {
+			totalSum += num;
+		}
+		dfs(0, 0, totalSum / k, input, k, new ArrayList<>(), result);
 		return result;
 	}
-	/**
-	 * do dfs and add to result
-	 * @param chunkIndex: the index of chunk we are at
-	 * @param index: the index of input array we are at
-	 * @param sum: the sum of each chunk. If sum == MIN_VALUE, that means we are at the first chunk
-	 * @param k: total number of chunk we need
-	 * @param input: the input array
-	 * @param cur: our current result
-	 * @param result: the final result
-	 */
-	private void dfs(int chunkIndex, int index, int sum, int[] input, int k, List<List<Integer>> cur, List<List<List<Integer>>> result) {
+
+	private void dfs(int chunkIndex, int arrayIndex, int targetSum, int[] input, int k, List<Integer> cur, List<List<Integer>> result) {
 		// base case
-		if (chunkIndex == k) {
-			if (index == input.length) {
-				// do a deep copy and add cur to result
-				List<List<Integer>> copy = new ArrayList<>();
-				for (List<Integer> list : cur) {
-					copy.add(new ArrayList<>(list));
-				}
-				result.add(copy);
+		if (chunkIndex == k || arrayIndex == input.length) {
+			if (chunkIndex == k && arrayIndex == input.length) {
+				result.add(new ArrayList<>(cur));
 			}
 			return;
 		}
 		int curSum = 0;
-		List<Integer> chunk = new ArrayList<>();
-		cur.add(chunk);
-		for (int i = index; i <= input.length - k + chunkIndex; i++) {
+		for (int i = arrayIndex; i <= input.length - k + chunkIndex; i++) {
 			curSum += input[i];
-			chunk.add(input[i]);
-			if (sum == Integer.MIN_VALUE || curSum == sum) { // if sum is MIN_VALUE, we are at the first chunk
-				dfs(chunkIndex + 1, i + 1, curSum, input, k, cur, result);
+			if (curSum == targetSum) {
+				cur.add(i);
+				dfs(chunkIndex + 1, i + 1, targetSum, input, k, cur, result);
+				cur.remove(cur.size() - 1);
 			}
 		}
-		cur.remove(cur.size() - 1);
 	}
 }
--- src/dfs/SplitArrayWithSameSum2.java
+++ src/dfs/SplitArrayWithSameSum2.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 /*
 Given an input array with integers, how to split the array into k subarrays such that each subarray shares the same sum.
-The subarrays cannot be adjacent to each other
+The subarrays cannot be adjacent to each other.
+If there are multiple valid results, only return one of them.
 
 
     3, -1,| (1, ,1 ,1, 2, 2)| 4, 6, -8,| 1, 1, k=3
@@ -18,29 +19,43 @@ dfs strategy
 
 recursion tree:
 number of levels: k
-each node has up to n - k + level number of branches
+each level: use nested for loop to get all possible subarrays for ith chunk
 
-base case: level == k
+						root
+			/  |   |   |   |   \
+		[0,0] [0,1] [0,2] ... [0,n-k], [1,1],[1,2]...[1,n-k] ... [n-k,n-k]        - n ^ 2
+			/  |  |  |  \
+		[2,2],[2,3]..[n-k+1,n-k+1]			                                      - (n-1) ^ 2
+
+
+base case: level == k || end of array is reached
 at each level: use a loop to keep adding elements, when current sum == target sum, do recursion
 Note: at first chunk, we do not have a sum, so we use MIN_VALUE as a special value to indicate the first chunk
+
+Return:
+list of list, each inner list has two numbers to represent the start and end indexes of each subarray
+
+Time: k * n ^ 2
+Space: n
+
  */
 public class SplitArrayWithSameSum2 {
 	public static void main(String[] args) {
 		SplitArrayWithSameSum2 solution = new SplitArrayWithSameSum2();
 		// test case 1
-		int[] input = new int[]{3, -1, 4, 6, -8, 1, 1};
+		int[] input = new int[]{3, -1, 1, 1, 1, 4, 6, -8, 1, 1};
 		int k = 3;
-		List<List<List<Integer>>> result = solution.splitArray(input, k);
+		List<List<Integer>> result = solution.splitArray(input, k);
 		System.out.println(result);
 
 		// test case 2
-//		int[] input2 = new int[]{0, 0, 0, 0, 0};
-//		int k2 = 3;
-//		List<List<List<Integer>>> result2 = solution.splitArray(input2, k2);
-//		System.out.println(result2);
+		int[] input2 = new int[]{0, 0, 0, 0, 0};
+		int k2 = 3;
+		List<List<Integer>> result2 = solution.splitArray(input2, k2);
+		System.out.println(result2);
 	}
-	public List<List<List<Integer>>> splitArray(int[] input, int k) {
-		List<List<List<Integer>>> result = new ArrayList<>();
+	public List<List<Integer>> splitArray(int[] input, int k) {
+		List<List<Integer>> result = new ArrayList<>();
 		if (input == null || input.length < k || k <= 1) {
 			return result;
 		}
@@ -48,46 +63,30 @@ public class SplitArrayWithSameSum2 {
 		for (int i = 1; i < prefixSum.length; i++) {
 			prefixSum[i] = prefixSum[i - 1] + input[i - 1];
 		}
-		dfs(0, 0, Integer.MIN_VALUE, input, prefixSum, k, new ArrayList<List<Integer>>(), result);
+		dfs(0, 0, Integer.MIN_VALUE, prefixSum, k, result);
 		return result;
 	}
-	/**
-	 * do dfs and add to result
-	 * @param chunkIndex: the index of chunk we are at
-	 * @param index: the index of input array we are at
-	 * @param sum: the sum of each chunk. If sum == MIN_VALUE, that means we are at the first chunk
-	 * @param k: total number of chunk we need
-	 * @param input: the input array
-	 * @param cur: our current result
-	 * @param result: the final result
-	 */
-	private void dfs(int chunkIndex, int index, int sum, int[] input, int[] prefixSum, int k, List<List<Integer>> cur, List<List<List<Integer>>> result) {
+
+	private boolean dfs(int chunkIndex, int arrayIndex, int targetSum, int[] prefixSum, int k, List<List<Integer>> result) {
 		if (chunkIndex == k) {
-			List<List<Integer>> copy = new ArrayList<>();
-			for (List<Integer> list : cur) {
-				copy.add(new ArrayList<>(list));
-			}
-			result.add(copy);
-			return;
+			return true;
 		}
-		boolean needRemove = false;
-		for (int i = index; i < prefixSum.length - k + chunkIndex; i++) {
+		for (int i = arrayIndex; i < prefixSum.length - k + chunkIndex; i++) {
 			for (int j = i + 1; j <= prefixSum.length - k + chunkIndex; j++) {
 				int curSum = prefixSum[j] - prefixSum[i];
-				if (sum != Integer.MIN_VALUE && curSum != sum) {
+				if (targetSum != Integer.MIN_VALUE && curSum != targetSum) {
 					continue;
 				}
 				List<Integer> chunk = new ArrayList<>();
-				cur.add(chunk);
-				needRemove = true;
-				for (int m = i + 1; m <= j; m++) {
-					chunk.add(input[m - 1]);
+				chunk.add(i);
+				chunk.add(j - 1);
+				result.add(chunk);
+				if (dfs(chunkIndex + 1, j, curSum, prefixSum, k, result)) {
+					return true;
 				}
-				dfs(chunkIndex + 1, j, curSum, input, prefixSum, k, cur, result);
+				result.remove(result.size() - 1);
 			}
 		}
-		if (needRemove) {
-			cur.remove(cur.size() - 1);
-		}
+		return false;
 	}
 }
